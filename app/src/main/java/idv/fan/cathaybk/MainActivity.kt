4af82c6f2473c735fe74016bedd1a8baf1c5813e
EvanHsu <a10033135@gmail.com>
2021-12-04 19:08:35 +0800

    Initial commit

---
+++ app/src/main/java/idv/fan/cathaybk/MainActivity.kt
@@ -0,0 +1,11 @@
+package idv.fan.cathaybk
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
